41a24409ea8f6235f5a9c189fbdb81814067a0dc
Leonardo Matos <leomp120894@gmail.com>
2018-03-08 17:56:27 -0300

    first steps with HTTP verbs

---
+++ routes/#modules.js
@@ -0,0 +1,63 @@
+'use strict'
+
+// log on files
+// const logger = require('./../lib/Logger.js')
+
+// JSON Schema validation with AJV
+// based on http://json-schema.org/
+const Ajv = require('ajv') // version >= 2.0.0
+const localize = require('ajv-i18n')
+// option `i18n` is required for this package to work
+const ajv = Ajv({ allErrors: true })
+// https://github.com/epoberezkin/ajv-i18n
+
+// Node raw HTTP module
+// const http = require('http')
+
+function ajvErrorHandling (errors, respond, coll) {
+  let moreInfo = '/' + coll + '/schema.json'
+  let devMsg = 'Bad-formatted JSON body (POST) or URL query params (GET), details in user_message'
+  let usrMsg = {
+    'en_us': ajv.errorsText(errors, { separator: '\n' })
+  }
+  // translate
+  localize['pt-BR'](errors)
+  usrMsg.pt_br = ajv.errorsText(errors, { separator: '\n' })
+
+  respond({}, null, 400, 'MOD901', devMsg, usrMsg, moreInfo)
+}
+
+function runModule (obj, respond, config, storeId, modName, validate) {
+  // ajv
+  let valid = validate(obj)
+  if (!valid) {
+    ajvErrorHandling(validate.errors, respond)
+  } else {
+    // proceed to modules host
+  }
+}
+
+function post ([ id, , body, respond, config, storeId ], modName, validate) {
+  // run module with JSON body as object
+  runModule(body, respond, config, storeId, modName, validate)
+}
+
+function get ([ id, meta, , respond, config, storeId ], modName, validate, schema) {
+  if (id) {
+    if (id === 'schema') {
+      // return JSON Schema
+      respond(schema)
+    } else {
+      let devMsg = 'Resource ID is acceptable only to JSON schema, at /' + modName + '/schema.json'
+      respond({}, null, 406, 'MOD101', devMsg)
+    }
+  } else {
+    // run module with query params as object
+    runModule(meta.query, respond, config, storeId, modName, validate)
+  }
+}
+
+module.exports = {
+  'get': get,
+  'post': post
+}
