354de3e19174491a45c312b1223eb053a11fb4ce
Leonardo Matos <leomp120894@gmail.com>
2019-03-25 18:21:49 -0300

    test: debug transaction app object

--- routes/#checkout.js
+++ routes/#checkout.js
@@ -205,18 +205,21 @@ module.exports = (checkoutBody, checkoutRespond, storeId) => {
                   })
 
                   // setup transaction app object
-                  transaction.app = { _id: transaction._id }
-                  // complete app object with some request body fields
-                  ;[
-                    'label',
-                    'icon',
-                    'intermediator',
-                    'payment_url'
-                  ].forEach(field => {
-                    if (checkoutBody.transaction.hasOwnProperty(field)) {
-                      transaction.app[field] = checkoutBody.transaction[field]
-                    }
-                  })
+                  if (!transaction.app) {
+                    transaction.app = { _id: transaction._id }
+                    // complete app object with some request body fields
+                    ;[
+                      'label',
+                      'icon',
+                      'intermediator',
+                      'payment_url'
+                    ].forEach(field => {
+                      if (checkoutBody.transaction.hasOwnProperty(field)) {
+                        transaction.app[field] = checkoutBody.transaction[field]
+                      }
+                    })
+                    logger.log(transaction.app)
+                  }
 
                   // merge transaction body with order info and respond
                   checkoutRespond({
